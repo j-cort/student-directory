42229fc5c35e2e374181040e0632d091864de5a3
j-cort <81960011+j-cort@users.noreply.github.com>
2021-11-12 22:11:49 +0000

    add input validation and default values

---
+++ directory.rb
@@ -0,0 +1,77 @@
+def input_students
+  # create an empty array
+  students = []
+  # Repeat this code until no name is given
+  while true do
+    # list of acceptable letters for villain names to begin with
+    acceptable_first_letters = ["D", "G", "I", "J", "M", "R", "S", "T", "X", "Z"]
+    
+    # list of acceptable cohort months 
+    acceptable_month = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+    
+    # is name acceptable
+    name_ok = false
+    
+    # get name
+    while !name_ok do
+      puts name_ok
+      puts "Enter student name"
+      puts "To finish, just hit return twice"
+      name = gets.chomp
+      
+      # validate name
+      if !name.empty? && !acceptable_first_letters.include?(name[0]) && name.length > 11
+        puts "Name must begin with (D, G, I, J, M, R, S, T, X, Z)"
+        puts "Name must be fewer than 12 characters"
+      elsif !name.empty? && !acceptable_first_letters.include?(name[0])
+        puts "Name must begin with (D, G, I, J, M, R, S, T, X, Z)"
+      elsif !name.empty? && name.length > 11
+        puts "Name must be fewer than 12 characters"
+      else   
+        name_ok = true
+      end
+    end
+    
+    
+    # break loop if name input is empty
+    if name.empty?
+      break
+    else
+      
+      # get cohort  
+      puts "Enter cohort"
+      cohort = gets.chomp.downcase
+      acceptable_month.include?(cohort) ? cohort = cohort.downcase.to_sym : cohort = :unknown
+      
+      # get weapon
+      puts "Enter weapoon"
+      weapon = gets.chomp
+      weapon.empty? ? weapon = :unknown : weapon = weapon.downcase.to_sym 
+      
+      # add the villain hash to array
+      students << {name: name, cohort: cohort, weapon: weapon}
+    end
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "----------"
+end
+
+def print(students)
+  students.each_with_index do |student, number|
+    puts "#{number + 1}. #{student[:name]}, #{student[:weapon]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
